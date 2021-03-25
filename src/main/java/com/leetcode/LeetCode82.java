1180db2202e7a7f53f4d99671c681bdd6f0e5f86
nanjing <p375242380@163.com>
2021-03-25 21:09:41 +0800

    82

---
+++ src/main/java/com/leetcode/LeetCode82.java
@@ -0,0 +1,52 @@
+package com.leetcode;
+
+import com.ListNode;
+
+/**
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ * <p>
+ * 返回同样按升序排列的结果链表。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,1,1,2,3]
+ * 输出：[2,3]
+ *
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
