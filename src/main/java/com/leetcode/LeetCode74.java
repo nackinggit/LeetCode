52b2af1448eba2203214970954df307019cbe835
nanjing <p375242380@163.com>
2021-03-30 22:37:06 +0800

    74

---
+++ src/main/java/com/leetcode/LeetCode74.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{-8,-7,-5,-3,-3,-1,1},{2,2,2,3,3,5,7},{8,9,11,11,13,15,17},{18,18,18,20,20,20,21},{23,24,26,26,26,27,27},{28,29,29,30,32,32,34}};
+        System.out.println(new LeetCode74().searchMatrix(matrix, -5));
+    }
+}
