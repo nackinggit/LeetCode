79fefa5ccfaf295ca1d7a934fff0765e848aba06
nanjing <p375242380@163.com>
2021-04-06 23:46:55 +0800

    some

---
+++ src/main/java/com/cxymsjd/C17_21.java
@@ -0,0 +1,43 @@
+package com.cxymsjd;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个直方图(也称柱状图)，假设有人从上面源源不断地倒水，最后直方图能存多少水量?直方图的宽度为 1。
+ * <p>
+ * <p>
+ * <p>
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的直方图，在这种情况下，可以接 6 个单位的水（蓝色部分表示水）。感谢 Marcos 贡献此图。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/volume-of-histogram-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class C17_21 {
+    public int trap(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int w = i - left - 1;
+                int h = Math.min(height[left], height[i]) - height[top];
+                ans += w * h;
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
