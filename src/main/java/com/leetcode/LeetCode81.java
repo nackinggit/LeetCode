8cde90d15e187462c69c800deb10c88325c59ad7
nanjing <p375242380@163.com>
2021-04-07 21:45:03 +0800

    33 81

---
+++ src/main/java/com/leetcode/LeetCode81.java
@@ -0,0 +1,70 @@
+package com.leetcode;
+
+/**
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 0
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 3
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * 题目数据保证 nums 在预先未知的某个下标上进行了旋转
+ * -104 <= target <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode81 {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+
+        int s = 0, e = n - 1;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (nums[m] == target) {
+                return true;
+            }
+
+            if (nums[m] == nums[s] && nums[m] == nums[e]) {
+                s++;
+                e--;
+            } else if (nums[s] <= nums[m]) {
+                if (nums[s] <= target && nums[m] > target) {
+                    e = m - 1;
+                } else {
+                    s = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[n - 1]) {
+                    s = m + 1;
+                } else {
+                    e = m - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
