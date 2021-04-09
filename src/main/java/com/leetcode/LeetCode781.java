864237b3f0ac65a752615d3f67aa0f0896619743
nanjing <p375242380@163.com>
2021-04-09 22:54:37 +0800

    781

---
+++ src/main/java/com/leetcode/LeetCode781.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 森林中，每个兔子都有颜色。其中一些兔子（可能是全部）告诉你还有多少其他的兔子和自己有相同的颜色。我们将这些回答放在 answers 数组里。
+ * <p>
+ * 返回森林中兔子的最少数量。
+ * <p>
+ * 示例:
+ * 输入: answers = [1, 1, 2]
+ * 输出: 5
+ * 解释:
+ * 两只回答了 "1" 的兔子可能有相同的颜色，设为红色。
+ * 之后回答了 "2" 的兔子不会是红色，否则他们的回答会相互矛盾。
+ * 设回答了 "2" 的兔子为蓝色。
+ * 此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。
+ * 因此森林中兔子的最少数量是 5: 3 只回答的和 2 只没有回答的。
+ * <p>
+ * 输入: answers = [10, 10, 10]
+ * 输出: 11
+ * <p>
+ * 输入: answers = []
+ * 输出: 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rabbits-in-forest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode781 {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int y : answers) {
+            count.put(y, count.getOrDefault(y, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int y = entry.getKey(), x = entry.getValue();
+            ans += (x + y) / (y + 1) * (y + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode781().numRabbits(new int[]{0, 0, 1, 1, 1}));
+    }
+}
