2640de3360a0e9f73a68902564fef8396a4627a8
nanjing <p375242380@163.com>
2021-04-11 19:24:03 +0800

    264

---
+++ src/main/java/com/leetcode/LeetCode264.java
@@ -0,0 +1,52 @@
+package com.leetcode;
+
+/**
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ * <p>
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 10
+ * 输出：12
+ * 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：1
+ * 解释：1 通常被视为丑数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode264 {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(num2, Math.min(num3, num5));
+            if (dp[i] == num2) {
+                p2++;
+            }
+
+            if (dp[i] == num3) {
+                p3++;
+            }
+
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode264().nthUglyNumber(10));
+    }
+}
