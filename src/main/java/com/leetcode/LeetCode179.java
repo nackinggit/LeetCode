e48010754c175e2090b5fd21aecd0ee235236f43
nanjing <p375242380@163.com>
2021-04-12 23:36:12 +0800

    179

---
+++ src/main/java/com/leetcode/LeetCode179.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class LeetCode179 {
+    public String largestNumber(int[] nums) {
+        Integer[] inums = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(inums, (o1, o2) -> {
+            String s1 = o1.toString();
+            String s2 = o2.toString();
+            return (s2 + s1).compareTo(s1 + s2);
+        });
+
+        System.out.println(Arrays.toString(inums));
+        if (inums[0] == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : inums) {
+            sb.append(i);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode179().largestNumber(new int[]{3,30,34,5,9}));
+    }
+}
