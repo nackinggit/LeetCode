6a8a07d011d006468d84d17155d91e36fba3c00f
nanjing <p375242380@163.com>
2021-06-02 21:37:11 +0800

    523

---
+++ src/main/java/com/leetcode/LeetCode523.java
@@ -0,0 +1,59 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+ * <p>
+ * 子数组大小 至少为 2 ，且
+ * 子数组元素总和为 k 的倍数。
+ * 如果存在，返回 true ；否则，返回 false 。
+ * <p>
+ * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [23,2,4,6,7], k = 6
+ * 输出：true
+ * 解释：[2,4] 是一个大小为 2 的子数组，并且和为 6 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [23,2,6,4,7], k = 6
+ * 输出：true
+ * 解释：[23, 2, 6, 4, 7] 是大小为 5 的子数组，并且和为 42 。
+ * 42 是 6 的倍数，因为 42 = 7 * 6 且 7 是一个整数。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [23,2,6,4,7], k = 13
+ * 输出：false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/continuous-subarray-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode523 {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int len;
+        if (nums == null || (len = nums.length) < 2) return false;
+
+        Map<Integer, Integer> preSum = new HashMap<>();
+        int reminder = 0;
+        preSum.put(0, -1);
+        for (int i = 0; i < len; i++) {
+            reminder = (reminder + nums[i]) % k;
+            if (preSum.containsKey(reminder)) {
+                int preIndex = preSum.get(reminder);
+                if (i - preIndex >= 2) {
+                    return true;
+                }
+            } else {
+                preSum.put(i, reminder);
+            }
+        }
+
+        return false;
+    }
+}
