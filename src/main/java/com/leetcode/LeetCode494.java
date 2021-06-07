97e17a71e311e3005c365f7fc125ceff70ffcfa0
nanjing <p375242380@163.com>
2021-06-07 22:04:44 +0800

    494

---
+++ src/main/java/com/leetcode/LeetCode494.java
@@ -0,0 +1,109 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 target 。
+ * <p>
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * <p>
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 20
+ * 0 <= nums[i] <= 1000
+ * 0 <= sum(nums[i]) <= 1000
+ * -1000 <= target <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/target-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode494 {
+    public int findTargetSumWays3(int[] nums, int target) {
+        int sum = Arrays.stream(nums).sum();
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+
+        int len = nums.length, neg = diff / 2;
+
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int i = neg; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[neg];
+    }
+
+    public int findTargetSumWays2(int[] nums, int target) {
+        int sum = Arrays.stream(nums).sum();
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+
+        int len = nums.length, neg = diff / 2;
+        int[][] dp = new int[len + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[len][neg];
+    }
+
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return count;
+
+        backtrace(nums, 0, target);
+        return count;
+    }
+
+    private void backtrace(int[] nums, int index, int curSum) {
+        if (index == nums.length) {
+            if (curSum == 0) {
+                count += 1;
+            }
+            return;
+        }
+
+        backtrace(nums, index + 1, curSum + nums[index]);
+        backtrace(nums, index + 1, curSum - nums[index]);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 1, 1};
+        System.out.println(new LeetCode494().findTargetSumWays2(nums, 3));
+    }
+}
