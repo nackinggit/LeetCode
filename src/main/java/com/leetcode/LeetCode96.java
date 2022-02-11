f9966bc454b475f91df9c2eba535205c77270c14
nanjing <p375242380@163.com>
2022-02-11 22:04:10 +0800

    96

---
+++ src/main/java/com/leetcode/LeetCode96.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+/**
+ * 96. 不同的二叉搜索树
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：n = 3
+ * 输出：5
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 19
+ */
+public class LeetCode96 {
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+
+        return G[n];
+    }
+}
