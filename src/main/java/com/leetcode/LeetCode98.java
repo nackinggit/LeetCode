78a1ef97f9864cb071c428ff1189e99b048e04bc
pengnanjing <p375242380@163.com>
2022-02-13 17:25:50 +0800

    98

---
+++ src/main/java/com/leetcode/LeetCode98.java
@@ -0,0 +1,73 @@
+package com.leetcode;
+
+import com.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+/**
+ * 98. 验证二叉搜索树
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 有效 二叉搜索树定义如下：
+ * <p>
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目范围在[1, 104] 内
+ * -231 <= Node.val <= 231 - 1
+ */
+public class LeetCode98 {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+
+        if (root.val >= max || root.val <= min) return false;
+
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        long pre = Long.MIN_VALUE;
+
+        if (root == null) return true;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            TreeNode node = stack.pop();
+            if(pre >= node.val) return false;
+            pre = node.val;
+            cur = node.right;
+        }
+
+        return true;
+    }
+}
