cafd69c21090b4bf80e26602fbaa8cbb224282dd
nanjing <p375242380@163.com>
2022-05-25 13:15:28 +0800

    some new leetcode

---
+++ src/main/java/com/leetcode/LeetCode207.java
@@ -0,0 +1,78 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * 207. 课程表
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ * <p>
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+ * <p>
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：true
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+ * 示例 2：
+ * <p>
+ * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * 输出：false
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= numCourses <= 105
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * prerequisites[i] 中的所有课程对 互不相同
+ */
+public class LeetCode207 {
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> requires = new HashMap<>();
+        for(int[] pre : prerequisites) {
+            Set<Integer> set = requires.getOrDefault(pre[0], new HashSet<>());
+            set.add(pre[1]);
+            requires.put(pre[0], set);
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(!requires.containsKey(i)) {
+                deque.add(i);
+            }
+        }
+
+        while(!deque.isEmpty()) {
+            int i = deque.poll();
+            for(int k : requires.keySet()) {
+                Set<Integer> set = requires.get(k);
+                if(set != null && !set.isEmpty()) {
+                    set.remove(i);
+                    if(set.isEmpty()) {
+                        deque.add(k);
+                    }
+                }
+            }
+        }
+
+        for (Set<Integer> set : requires.values()) {
+            if(!set.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] prerequisites = {{1, 0}};
+        System.out.println(canFinish(2, prerequisites));
+    }
+}
--- src/main/java/com/leetcode/LeetCode297.java
+++ src/main/java/com/leetcode/LeetCode297.java
@@ -1,46 +1,75 @@
 package com.leetcode;
 
+import com.TreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * 给定正整数n，找到若干个完全平方数（比如1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * 297. 二叉树的序列化与反序列化
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
  * <p>
- * 给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+ * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
  * <p>
- * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ * 提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
  * <p>
- *
  * <p>
- * 示例1：
  * <p>
- * 输入：n = 12
- * 输出：3
- * 解释：12 = 4 + 4 + 4
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
  * 示例 2：
  * <p>
- * 输入：n = 13
- * 输出：2
- * 解释：13 = 4 + 9
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ * 示例 4：
+ * <p>
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ * <p>
  * <p>
  * 提示：
  * <p>
- * 1 <= n <= 104
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/perfect-squares
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 树中结点数在范围 [0, 104] 内
+ * -1000 <= Node.val <= 1000
  */
 public class LeetCode297 {
+    public String serialize(TreeNode root) {
+        return doSerialize(root, "");
+    }
 
-    public int numSquares(int n) {
-        int[] f = new int[n + 1];//f[i]表示i的完全平方数的最小数
-        f[0] = 0;
-        f[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int minC = Integer.MAX_VALUE;
-            for (int j = 1; j * j <= i; j++) {
-                minC = Math.min(minC, f[i - j * j]);
-            }
-            f[i] = minC + 1;
+    private String doSerialize(TreeNode root, String s) {
+        if(root == null) {
+            s += "NIL,";
+        } else {
+            s += root.val + ",";
+            s = doSerialize(root.left, s);
+            s = doSerialize(root.right, s);
         }
-        return f[n];
+        return s;
+    }
+
+    public TreeNode deserialize(String data) {
+        List<String> dataList = Arrays.asList(data.split(","));
+        return doDeserialize(dataList);
+    }
+
+    private TreeNode doDeserialize(List<String> dataList) {
+        if(dataList.get(0).equals("NIL")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        node.left = doDeserialize(dataList);
+        node.right = doDeserialize(dataList);
+        return node;
     }
 }
