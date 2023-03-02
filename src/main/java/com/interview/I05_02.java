a95d7f30354214cab9cd0e5f6c05a3812d681c23
nanjing <p375242380@163.com>
2023-03-02 21:30:16 +0800

    daily :# Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/interview/I05_02.java
@@ -0,0 +1,38 @@
+package com.interview;
+
+/**
+ * 面试题 05.02. 二进制数转字符串
+ * 提示
+ * 中等
+ * 123
+ * 相关企业
+ * 二进制数转字符串。给定一个介于0和1之间的实数（如0.72），类型为double，打印它的二进制表达式。如果该数字无法精确地用32位以内的二进制表示，则打印“ERROR”。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：0.625
+ * 输出："0.101"
+ * 示例2:
+ * <p>
+ * 输入：0.1
+ * 输出："ERROR"
+ * 提示：0.1无法被二进制准确表示
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 32位包括输出中的 "0." 这两位。
+ * 题目保证输入用例的小数位数最多只有 6 位
+ */
+public class I05_02 {
+    public String printBin(double num) {
+        StringBuilder sb = new StringBuilder("0");
+        while (sb.length() <= 32 && num != 0) {
+            num = num * 2;
+            int c = (int) num;
+            sb.append(c);
+            num -= c;
+        }
+        return sb.length() <= 32 ? sb.toString() : "ERROR";
+    }
+}
